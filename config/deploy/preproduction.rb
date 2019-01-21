5a4a1339296e06451f06f5593397b60fe7c4b576
waiyanmin1001 <waiyanmin@code2lab.com>
2019-01-21 17:43:40 +0630

    preproduction

---
+++ config/deploy/preproduction.rb
@@ -0,0 +1,14 @@
+set :stage, :production
+set :branch, "master"
+set :rbenv_ruby, '2.4.1'
+set :deploy_to, "/home/ubuntu/#{fetch(:application)}_#{fetch(:stage)}"
+set :default_env, {
+  'app_dir' => '#{fetch(:deploy_to)}',
+  'RAILS_ENV' => 'production'
+}
+server '13.229.119.187', user: 'ubuntu', roles: %w{web app db}, primary: true
+
+set :rails_env, 'production'
+
+set :puma_bind, %w(tcp://0.0.0.0:9393)
+set :puma_workers, 2
